d4f407badf73e0ecc22d3174b8e69dc9d54eb9d6
Barsik1976 <37069851+Barsik1976@users.noreply.github.com>
2021-05-21 12:46:43 +0200

    Add files via upload

--- js/script-gallery.js
+++ js/script-gallery.js
@@ -1,16 +1,18 @@
     var galleryThumbs = new Swiper('.slider-product__thumbs', {
         direction: "vertical",
-        slidesPerView: 1,
+        spaceBetween: 24,
+        slidesPerView: 2,
         loop: true,
         freeMode: true,
-    loopedSlides: 1, //looped slides should be the same
+    loopedSlides: 2, //looped slides should be the same
     watchSlidesVisibility: true,
     watchSlidesProgress: true,
 });
 
     var galleryBig = new Swiper('.slider-big', {
+        spaceBetween: 24,
         loop: true,
-    loopedSlides: 1, //looped slides should be the same
+    loopedSlides: 2, //looped slides should be the same
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
